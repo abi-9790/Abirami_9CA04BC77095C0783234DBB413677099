7f7c84422cf8ac0162f00443455de480aa6c25f7
abi-9790 <144480263+abi-9790@users.noreply.github.com>
2023-09-19 22:49:31 +0530

    Add files via upload

---
+++ challenge unit 2/challenge 2.2.py
@@ -0,0 +1,24 @@
+class Player:
+
+   def play(self): 
+  
+       print("The Player is playing Cricket.")
+
+class Batsman(Player):
+
+   def play(self):
+
+        print("The Batsman is batting")
+
+class Bowler(Player):
+
+   def play(self):
+      
+         print("The Bowler is bowling")
+
+batsman = Batsman()
+
+bowler = Bowler()
+
+batsman.play()
+bowler.play()
